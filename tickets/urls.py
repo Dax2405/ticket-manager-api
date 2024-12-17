7248ff73537ccf5183f30b0d01f513ef9847d0e6
Dax <daxnavarrete@gmail.com>
2024-12-17 17:39:53 -0500

    update endpoints

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from .views import EnterpriseListCreateView, PlaceListCreateView, TicketListCreateView, VerifyTicketView, ExitTicketView, AddPhotoView
+from .views import EnterpriseListCreateView, PlaceListCreateView, TicketListCreateView, LogsListCreateView, VerifyTicketView, ExitTicketView, AddPhotoView, EnterTicketView, InPlaceNumberView
 
 urlpatterns = [
-    path('enterprises/', EnterpriseListCreateView.as_view(),
+    path('enterprises', EnterpriseListCreateView.as_view(),
          name='enterprise_list_create'),
-    path('places/', PlaceListCreateView.as_view(), name='place_list_create'),
-    path('tickets/', TicketListCreateView.as_view(), name='ticket_list_create'),
-    path('verify-ticket/', VerifyTicketView.as_view(), name='verify_ticket'),
-    path('exit-ticket/', ExitTicketView.as_view(), name='exit_ticket'),
-    path('add-photo/', AddPhotoView.as_view(), name='add_photo'),
+    path('places', PlaceListCreateView.as_view(), name='place_list_create'),
+    path('ticket', TicketListCreateView.as_view(), name='ticket_list_create'),
+    path('verify-ticket', VerifyTicketView.as_view(), name='verify_ticket'),
+    path('exit-ticket', ExitTicketView.as_view(), name='exit_ticket'),
+    path('add-photo', AddPhotoView.as_view(), name='add_photo'),
+    path('enter-ticket', EnterTicketView.as_view(), name='enter_ticket'),
+    path('logs', LogsListCreateView.as_view(), name='logs_list_create'),
+    path('in-place-number', InPlaceNumberView.as_view(), name='in_place_number'),
 
 ]
