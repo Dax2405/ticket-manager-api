88c9453ac6792d11661433ed1726c115317e16b0
Dax <daxnavarrete@gmail.com>
2024-12-17 17:39:19 -0500

    add logs support and improve responses

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Enterprise, Ticket, In_Place, Place
+from .models import Enterprise, Ticket, In_Place, Place, Logs
 
 
 class EnterpriseSerializer(serializers.ModelSerializer):
@@ -15,12 +15,43 @@ class PlaceSerializer(serializers.ModelSerializer):
 
 
 class TicketSerializer(serializers.ModelSerializer):
+    destination_name = serializers.SerializerMethodField()
+    enterprise_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Ticket
         fields = '__all__'
+
+    def get_destination_name(self, obj):
+        return obj.destination.name
+
+    def get_enterprise_name(self, obj):
+        return obj.enterprise.name
+
+    def validate_id_number(self, value):
+        if Ticket.objects.filter(id_number=value).exists():
+            raise serializers.ValidationError(
+                "A ticket with this ID number already exists.")
+        return value
 
 
 class InPlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = In_Place
         fields = '__all__'
+
+
+class LogsSerializer(serializers.ModelSerializer):
+    ticket_name = serializers.SerializerMethodField()
+    place_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Logs
+        fields = ['id', 'ticket', 'place', 'action',
+                  'time', 'ticket_name', 'place_name']
+
+    def get_ticket_name(self, obj):
+        return obj.ticket.name
+
+    def get_place_name(self, obj):
+        return obj.place.name
